bd8eb1027090ee389ca52bff3de482c1593233c4
Glauter Santos <glauterlima@gmail.com>
2017-04-03 15:51:22 -0300

    Commit angular material

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,19 @@
+angular
+  .module('alurapic')
+  .controller('FotosController', function($scope, recursoFoto) {
+    recursoFoto.query(function(fotos) {
+        $scope.fotos = fotos;
+      }, function(erro) {
+        console.error('As fotos não foram carregadas')
+      });
+    $scope.remover = function(foto) {
+      recursoFoto.delete({fotoId: foto._id}, function() {
+          var indiceDaFoto = $scope.fotos.indexOf(foto);
+          $scope.fotos.splice(indiceDaFoto, 1);
+          $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso';
+        }, function(erro) {
+          console.log(erro);
+          $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+        });
+    }
+});
